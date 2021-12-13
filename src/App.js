ad0df2a23d6894488baf50db1ec2afba6fb0b37b
jwiergowska <85780659+jwiergowska@users.noreply.github.com>
2021-12-13 13:27:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import WeatherForecast from "./WeatherForecast";
 const api = {
   key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
   base: "https://api.openweathermap.org/data/2.5/"
